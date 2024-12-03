153c176661c1bb135b5fee6af767755fb00d884d
Oleh Matiiuk <olgerd27@gmail.com>
2024-12-03 12:01:17 +0200

    Added and used set_tm() to populate tm structs

--- my_solutions/Ch01/problem1_my.c
+++ my_solutions/Ch01/problem1_my.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-void print_tm(const struct tm *tm_data, const char *comment)
-{
-  printf("%s:\n%4d-%02d-%02d %02d:%02d:%02d\n", 
-         comment, 
-	 tm_data->tm_year, tm_data->tm_mon, tm_data->tm_mday, 
-	 tm_data->tm_hour, tm_data->tm_min, tm_data->tm_sec);
-}
-
+// Do the parsing of the date & time strings and set the parsed values into the tm struct instance.
 int parse_datetime(const char *str_date, const char *str_time, struct tm *tm_res)
 {
   int nitems = sscanf(str_date, "%4d_%2d_%2d", &tm_res->tm_year, &tm_res->tm_mon, &tm_res->tm_mday);
@@ -17,23 +11,56 @@ int parse_datetime(const char *str_date, const char *str_time, struct tm *tm_res
   return nitems == 6 ? 0 : 1;
 }
 
-// TODO: change the correct data types instead of 'date' and 'time' in the function signature.
-//
-// Returns the slope coefficient of the barometric pressure.
+// Set the values of the tm struct instance in a way of parsing 
+// the passed date & time string-type arguments.
+void set_tm(const char *str_date, const char *str_time, struct tm *tm_res)
+{
+  if ( parse_datetime(str_date, str_time, tm_res) != 0 ) {
+    fprintf(stderr, 
+            "!--Error 2: cannot parse the date&time value.\ndate: '%s'\ntime: '%s'\n", 
+            str_date, str_time);
+    exit(2);
+  }
+}
+
+// Print to STDOUT the values of the tm struct instance.
+void print_tm(const struct tm *tm_data, const char *comment)
+{
+  printf("%s:\n%4d-%02d-%02d, %02d:%02d:%02d\n", 
+         comment, 
+         tm_data->tm_year, tm_data->tm_mon, tm_data->tm_mday, 
+         tm_data->tm_hour, tm_data->tm_min, tm_data->tm_sec);
+}
+
+// Calc the Slope coefficient of the barometric pressure.
 double coeff_slope_press(FILE *pf, const struct tm *tm_begin, const struct tm *tm_end)
 {
   const int LINE_MAX = 150; // the max line length in the dat files 
-  char buff[LINE_MAX]; // buffer for storage of the read line
+  char buff_line[LINE_MAX]; // buffer for storage of the read line
+  struct tm tm_curr; // the tm value read in the current line
+  time_t time_curr; // the time_t value in the current line
   double k_slope; // the slope coefficient
 
-  //const time_t time_begin = mktime((struct tm *)tm_begin);
-  //const time_t time_end = mktime((struct tm *)tm_end);
+  const time_t time_begin = mktime((struct tm *)tm_begin);
+  const time_t time_end = mktime((struct tm *)tm_end);
 
+  // Reading the file to retrieve the data for analysis
   int i = 0;
-  (void)fgets(buff, LINE_MAX, pf); // skip the header
-  while ( fgets(buff, LINE_MAX, pf) != NULL ) {
-    printf("%s", buff);
-    if (i++ > 5) break;
+  (void)fgets(buff_line, LINE_MAX, pf); // skip the header
+  while ( fgets(buff_line, LINE_MAX, pf) != NULL ) {
+    set_tm(buff_line, strchr(buff_line, ' '), &tm_curr); 
+    print_tm(&tm_curr, "Current tm");
+
+    // Convert the current tm value to time_t
+    if ((time_curr = mktime(&tm_curr)) == -1) {
+      fprintf(stderr, "!---Error 3: wrong datetime value in the dat-file:\n'%s'\n", buff_line);
+      exit(3);
+    }
+
+    // Compare
+    //if (time_curr >= time_begin) 
+
+    if (++i > 5) break;
   }
   return k_slope;
 }
@@ -57,7 +84,7 @@ double coeff_slope_press(FILE *pf, const struct tm *tm_begin, const struct tm *t
  */
 int main(int argc, char *argv[])
 {
-  struct tm tm_begin, tm_end;
+  struct tm tm_begin = {0}, tm_end = {0};
   if (argc != 6) {
     fprintf(stderr, "Usage: %s weather_filename begin_date begin_time end_date end_time\n"
                     "where the following formats should be used:\n"
@@ -73,22 +100,12 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  // Parse the passed date & time arguments of the begin and end of the range for analysis
-  if ( parse_datetime(argv[2], argv[3], &tm_begin) != 0 ) {
-    fprintf(stderr, 
-            "!--Error 2: cannot parse the begin date&time value.\ndate: '%s'\ntime: '%s'\n", 
-	    argv[2], argv[3]);
-    exit(2);
-  }
-  
-  if ( parse_datetime(argv[4], argv[5], &tm_end) != 0 ) {
-    fprintf(stderr, 
-            "!--Error 2: cannot parse the beginning date&time value.\ndate: '%s'\ntime: '%s'\n", 
-	    argv[4], argv[5]);
-    exit(2);
-  }
-  //print_tm(&tm_begin, "The begin of the date & time range");
-  //print_tm(&tm_end, "The end of the date & time range");
+  // Set the the tm struct instances of the begin and end of the data range 
+  // (for further analysis) by parsing the passed arguments.
+  set_tm(argv[2], argv[3], &tm_begin);
+  set_tm(argv[4], argv[5], &tm_end);
+  // print_tm(&tm_begin, "The begin of the date & time range");
+  // print_tm(&tm_end, "The end of the date & time range");
   
   // Calc the Slope coefficient
   double k_press = coeff_slope_press(pfile, &tm_begin, &tm_end);
